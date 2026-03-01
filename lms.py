11071707e378c34fda7b3bad59e26c03470a3656
An Jaebeom <ajb8533296@gmail.com>
2026-03-01 18:38:26 +0900

    api url 변수 분리

--- lms.py
+++ lms.py
@@ -2,6 +2,7 @@ import requests
 
 class Lms:
     def __init__(self):
+        self.api_url = "https://lms.kumoh.ac.kr:81/api/v1"
         self.header = {
             "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36",
         }
@@ -15,7 +16,7 @@ class Lms:
         """
         Login to LMS
         """
-        res = requests.post("https://lms.kumoh.ac.kr:81/api/v1/signin", json={
+        res = requests.post(f"{self.api_url}/signin", json={
             "userNumber": username,
             "password": password
         }, headers=self.header).json()
@@ -24,39 +25,35 @@ class Lms:
         self.lms_user_id = res["lmsUserId"]
         self.name = res["name"]
         self.email = res["email"]
+
+        self.header = self.header | {
+            "Authorization": "Bearer " + self.access_token
+        }
     
     def get_notification(self):
         """
         공지사항 가져오기
         """
-        res = requests.get("https://lms.kumoh.ac.kr:81/api/v1/help/open-notification/all", headers=self.header | {
-            "Authorization": "Bearer " + self.access_token
-        })
+        res = requests.get(f"{self.api_url}/help/open-notification/all", headers=self.header)
         return res.json()
 
     def get_subject(self):
         """
         수강중인 과목 가져오기
         """
-        res = requests.get("https://lms.kumoh.ac.kr:81/api/v1/subject/users/me/enrollment/all", headers=self.header | {
-            "Authorization": "Bearer " + self.access_token
-        })
+        res = requests.get(f"{self.api_url}/subject/users/me/enrollment/all", headers=self.header)
         return res.json()
     
     def get_course_notice(self):
         """
         수강중인 과목에 대한 공지사항 가져오기
         """
-        res = requests.get("https://lms.kumoh.ac.kr:81/api/v1/home/users/me/notice/all", headers=self.header | {
-            "Authorization": "Bearer " + self.access_token
-        })
+        res = requests.get(f"{self.api_url}/home/users/me/notice/all", headers=self.header)
         return res.json()
 
     def get_student_info(self, id):
         """
         학생 정보 가져오기
         """
-        res = requests.get(f"https://lms.kumoh.ac.kr:81/api/v1/users?id={id}", headers=self.header | {
-            "Authorization": "Bearer " + self.access_token
-        })
+        res = requests.get(f"{self.api_url}/users?id={id}", headers=self.header)
         return res.json()
